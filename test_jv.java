d14c91254eaf465fa5b98232e94036095a657bfc
Mtptk <88386417+Mtptk@users.noreply.github.com>
2021-08-05 21:25:17 +0700

    add 666 in line 5

--- test_jv.java
+++ test_jv.java
@@ -2,7 +2,7 @@ public class test_jv {
     public static void main(String[] args) {
         a=10;
 
-        System.out.print("eie"); 
+        System.out.print("eie6666"); 
         System.out.print("ee55555"); 
     }
    
