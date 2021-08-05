90c1a48962bb4535f10c7016e77d717fb8e0ccb3
Mtptk <88386417+Mtptk@users.noreply.github.com>
2021-08-05 18:50:35 +0700

    Update test_jv.java

--- test_jv.java
+++ test_jv.java
@@ -1,7 +1,7 @@
 public class test_jv {
     public static void main(String[] args) {
         System.out.print("eie"); 
-        System.out.print("eie"); 
+        System.out.print("ei55555e"); 
     }
    
 }
